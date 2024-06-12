ea68a863e48ba25c4514376e884fc2f3263f8948
Saminouhass <nouhasssami23@gmail.com>
2024-06-12 13:12:09 +0100

    project v0.1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,73 @@
 #include <iostream>
 using namespace std;
 
+void login();
+void sing_up(); 
+void database();
+int i=1;
+string email,passWord1,passWord2;
+
+
 int main(){
-    cout << "Hello";
-    return 0;
+    bool work;
+    char choise;
+    cout << "|Hello in the sing page|" << endl;
+    while (true){
+        cout << "For start *1* or exit *0*: ";
+        cin >> work;
+        if(!work){
+            return 0;
+        } else if(work){
+            cout << "please enter your choise : "<< endl;
+            cout << "A.Sing-up"<< endl << "B.Login"<< endl;
+            cin >> choise;
+            if(choise == 'A'){
+                sing_up();
+            } else if (choise == 'B'){
+                login();
+                debug();
+                i++;
+            } else {
+                cout << "Err";
+            }
+        } else {
+            cout << "Err";
+        }
+    } 
+
+}
+
+void login(){
+    cout << "Login:"<< endl;
+    cout << "Email:";  
+    cin >> email;
+    cout << "Enter new Password :";
+    cin >> passWord1;
+    cout << "Verife your Password:";
+    cin >> passWord2;
+    while(passWord1 != passWord2){
+        cout << "Password don't match!" << endl;
+        cout << "Enter new Password :";
+        cin >> passWord1;
+        cout << "Verife your Password:";
+        cin >> passWord2;
+    } 
+    cout << "All is good" << endl;
+
+}
+
+void sing_up(){
+    
+}
+
+void database(){
+    string  user[i][2];
+    user[i][0] = email;
+    user[i][1] = passWord1;
+}
+
+void debug(){
+    for(int j=1;j<=i;j++){
+        cout << user[j][2];
+    }
 }
